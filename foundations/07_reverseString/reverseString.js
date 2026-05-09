75a917a871b7d5feca9ad570fe43758718c4dc79
KaashDev <seetohulkaash@gmail.com>
2026-05-09 16:57:12 -0400

    string manipulation

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if(word == ""){
+        return "";
+    }
 
+    reverse = "";
+
+    for(const char of word){
+        reverse = char + reverse;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
