897a171cc4a7a6527b698e9d9693452ba1435f10
KaashDev <seetohulkaash@gmail.com>
2026-05-09 17:17:11 -0400

    remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+     
+    for(let i = 0; i < values.length; i++){
+        while(array.includes(values[i])){
+            array.splice(array.indexOf(values[i]), 1);
+        }
+    }
+
+    return array;
+    
 };
 
 // Do not edit below this line
