8c71c68231297c8564075b3e609bf822a6a8a17b
KaashDev <seetohulkaash@gmail.com>
2026-05-21 13:25:07 -0400

    solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = {};
+    oldest.yearOfBirth = 2000;
+    oldest.yearOfDeath = 2000;
 
+    for(let person of people){
+
+        if(!(Object.hasOwn(person, "yearOfDeath"))){
+            person.yearOfDeath = new Date().getFullYear();
+        }   
+
+        if((person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)){
+            oldest = person;
+        }
+    }
+    return oldest;
+    
 };
 
 // Do not edit below this line
