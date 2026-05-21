55e61bd98998b2edd49e3a01ed6e9cb6dd71b6fc
KaashDev <seetohulkaash@gmail.com>
2026-05-21 12:53:16 -0400

    fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(typeof num ==='string') num = parseInt(num);
+    if(num < 0) return "OOPS";
 
+    let curr = 1;
+    let prev = 0;
+
+    for(let i = 0; i < num; i++){
+        let temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+    return prev;
 };
 
 // Do not edit below this line
