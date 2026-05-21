900274bef6ca6f67944a8b040b03bdcf9d991e28
KaashDev <seetohulkaash@gmail.com>
2026-05-21 12:40:18 -0400

    string manipulation

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanum ="qwertyuiopasdfghjklZxcvbnm1234567890";
 
+    const cleaned = word.toLowerCase()
+                        .split("")
+                        .filter(char => alphanum.includes(char))
+                        .join('');
+
+    return cleaned === cleaned.split('').reverse().join('');
 };
 
 // Do not edit below this line
